60417c295f15d3990031b3f0791ac528a4baf0c3
Alex S <alex.sunga@fliptech.co>
2018-12-18 10:07:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,19 +1,27 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
   
   new_hash = {}
   
-  #language style  
+    
   languages.each do |style, type|
-     
+    
     type.each do |language, description|
       
       if new_hash[language] == nil 
-          new_hash[language] = description
+         
+         new_hash[language] = description
+          
           new_hash[language][:style] = []
-        end 
+        
+        end
+        
     new_hash[language][:style] << style 
+    
     end 
+  
   end 
      
   new_hash
