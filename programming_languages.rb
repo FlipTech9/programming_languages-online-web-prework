44f5972f9af957200d8bc555a754c56727349dd3
Alex S <alex.sunga@fliptech.co>
2018-12-18 09:59:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,9 @@ def reformat_languages(languages)
   #language style  
   languages.each do |style, type|
     #lanuage name 
-    type.each do |language, des
+    type.each do |language, description|
+      
+    end 
   end 
      
   new_hash
